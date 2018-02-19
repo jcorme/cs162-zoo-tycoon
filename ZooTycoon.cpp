38cfe059eba3b656537888052a8b7fdd71334b49
Jason Chen <jason@jcndrop.com>
2018-02-18 17:35:34 -0800

    Add message at the beginning of the game

--- ZooTycoon.cpp
+++ ZooTycoon.cpp
@@ -1,8 +1,14 @@
-#include "PlayerAction.h"
+#include <iostream>
 #include "Game.h"
 
 int main() {
+  std::cout << "Welcome to Zoo Tycoon!\n"
+            << "Hit enter to start the game...";
+  std::cin.ignore();
+  std::cout << "\n\n" << std::endl;
+
   Game game;
   game.Run();
+
   return 0;
 }
